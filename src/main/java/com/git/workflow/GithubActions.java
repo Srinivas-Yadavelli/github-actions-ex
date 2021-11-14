046318886d7f98a7a123342ee58f354f4d3a707b
Srinivas-Yadavelli <techseen9@gmail.com>
2021-11-14 19:02:55 +0530

    intial commit

---
+++ src/main/java/com/git/workflow/GithubActions.java
@@ -0,0 +1,23 @@
+package com.git.workflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class GithubActions {
+
+	@GetMapping("/greet")
+	public String getGreeting()
+	{
+		return  "Welcome to greetings";
+
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubActions.class, args);
+	}
+
+}
